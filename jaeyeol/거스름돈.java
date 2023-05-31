9142259fba391e7b69ac761946b49b3758e1e485
jaeyeol <49369306+devjy39@users.noreply.github.com>
2023-06-01 01:57:00 +0900

    거스름돈
    https://school.programmers.co.kr/learn/courses/30/lessons/12907

---
+++ jaeyeol/거스름돈.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+/*
+*   기본적인 냅색 DP 문제
+*   1원부터 N원까지 모든 경우의 수를 코인을 순회하며 기록한다.
+*/
+
+
+class 거스름돈 {
+    static final int MOD = 1_000_000_007;
+
+    public int solution(int n, int[] money) {
+        Arrays.sort(money);
+        int[] dp = new int[n + 1];
+
+        for (int m : money) {
+            dp[m]++;
+            for (int i = m + 1; i <= n; i++) {
+                dp[i] = (dp[i] + dp[i - m]) % MOD;
+            }
+        }
+
+        return dp[n];
+    }
+}
